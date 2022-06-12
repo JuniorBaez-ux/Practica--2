5c837096202b202a139b9840f76b6a1c43aa6797
Junior Baez <junior_baez@ucne.edu.do>
2022-06-12 19:44:48 -0400

    Making an acurracy and cofusion matrix for predictions

--- script.js
+++ script.js
@@ -33,8 +33,12 @@ async function run() {
   const data = new MnistData();
   await data.load();
   await showExamples(data);
-  
+
   const model = getModel();
+
+  await showAccuracy(model, data);
+await showConfusion(model, data);
+
 tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
   
 await train(model, data);
@@ -136,5 +140,39 @@ function getModel() {
       callbacks: fitCallbacks
     });
   }
+
+  const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+function doPrediction(model, data, testDataSize = 1000) {
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax(-1);
+  const preds = model.predict(testxs).argMax(-1);
+
+  testxs.dispose();
+  return [preds, labels];
+}
+
+
+async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = {name: 'Accuracy', tab: 'Evaluation'};
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+  labels.dispose();
+}
+
+async function showConfusion(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  tfvis.render.confusionMatrix(container, {values: confusionMatrix, tickLabels: classNames});
+
+  labels.dispose();
+}
+
 
 document.addEventListener('DOMContentLoaded', run);
